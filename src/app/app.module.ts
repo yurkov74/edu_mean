2dbe0049814bca0a7d97f9bf6c7b4e4d738b2a63
Iuri Covaliov <iuri.covaliov@meta.md>
2020-08-22 15:28:02 +0200

    material package added header component configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+//material
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+// app modules
 import { AppRoutingModule } from './app-routing.module';
+// app components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +22,9 @@ import { PostListComponent } from './posts/post-list/post-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule, MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
