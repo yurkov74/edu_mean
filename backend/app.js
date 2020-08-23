9a7c67df979b35a97886373db8dc1d88303545b1
Iuri Covaliov <iuri.covaliov@meta.md>
2020-08-23 11:31:02 +0200

    backend initialized

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+
+const app = express();
+
+const mngConectionOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+mongoose
+  .connect(
+    "mongodb://localhost:27017/mean",
+    mngConectionOptions
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
+
+app.use("/api/posts", postsRoutes);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// });
+
+module.exports = app;
