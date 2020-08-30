c789bb3de4d79187db688f805bade64b20b118cf
Iuri Covaliov <iuri.covaliov@meta.md>
2020-08-30 22:49:34 +0200

    Section 08 interceptor with authorization token added to requests on the frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 //material
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
@@ -11,8 +11,11 @@ import {MatInputModule} from '@angular/material/input';
 import {MatPaginatorModule} from '@angular/material/paginator'
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatToolbarModule} from '@angular/material/toolbar';
+
 // app modules
 import { AppRoutingModule } from './app-routing.module';
+// app services
+import { AuthInterceptor } from './auth/auth-interceptor';
 // app components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -38,7 +41,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     HttpClientModule,
     MatButtonModule, MatCardModule, MatInputModule, MatExpansionModule, MatPaginatorModule, MatProgressSpinnerModule, MatToolbarModule,
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
